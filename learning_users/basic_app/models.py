88e15285c6986ba5a58a20a30d0579e343f6c5d5
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-08-25 20:10:35 +0200

    Whole project

---
+++ learning_users/basic_app/models.py
@@ -0,0 +1,12 @@
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    portfolio_site = models.URLField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    def __str__(self):
+        return self.user.username
